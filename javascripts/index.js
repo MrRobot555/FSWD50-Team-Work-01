7f498673edde88a14c1ff1a18e1151330d02fe30
nicoarl <nico.adelmann@hotmail.com>
2018-09-25 14:20:10 +0200

    some small changes

--- javascripts/index.js
+++ javascripts/index.js
@@ -7,8 +7,14 @@ function playAudio() {
 } 
 
 
-function redirectPage(){
-
-
-
-}
+$('.watch-video').click(function() { $('#full-page-video').fadeIn(); });
+$('.close-full-page-video').click(function() {
+var $frame = $('#full-page-video iframe');
+// saves the current iframe source
+var vidsrc = $frame.attr('src');
+// sets the source to nothing, stopping the video
+$frame.attr('src','');
+// sets it back to the correct link so that it reloads immediately on the next window open
+$frame.attr('src', vidsrc);
+$('#full-page-video').fadeOut();
+});
