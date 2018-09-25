3825fbe0f03e4ba9ad9a180a441fc035e9b34ccb
nicoarl <nico.adelmann@hotmail.com>
2018-09-25 13:15:31 +0200

    renamed some things & added some js

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,7 +1,11 @@
-
 document.getElementById("gramophon").addEventListener("click", playAudio);
 
 function playAudio() { 
     var sound = document.getElementById("sound"); 
+    document.log(sound);
     sound.play(); 
 } 
+
+function openBook(){
+	var SiteToOpen = document.getElementById("delia");
+}
